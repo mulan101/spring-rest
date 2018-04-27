d54b20a0ef8ab455611682b9e6c2447948f32cc0
jeho.han <jeho.han@DESKTOP-4RML614.hwk.net>
2018-04-27 10:40:36 +0900

    프로젝트 최초 생성

---
+++ src/main/java/com/jeho/demo/PrototypeRestApplication.java
@@ -0,0 +1,19 @@
+package com.jeho.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PrototypeRestApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeRestApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PrototypeRestApplication.class);
+	}
+}
