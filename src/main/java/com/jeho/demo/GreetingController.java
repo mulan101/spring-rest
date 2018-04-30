194c6cedee807a901bbbade77da43970fc054a8c
jeho.han <jeho.han@DESKTOP-4RML614.hwk.net>
2018-05-01 01:25:12 +0900

    111

--- src/main/java/com/jeho/demo/GreetingController.java
+++ src/main/java/com/jeho/demo/GreetingController.java
@@ -1,6 +1,9 @@
 package com.jeho.demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello, %s, %s";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    	InetAddress ip = null;
+		try {
+			ip = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                            String.format(template, name, ip.toString()));
     }
 }
